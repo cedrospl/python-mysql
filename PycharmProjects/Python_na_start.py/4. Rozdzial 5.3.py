9b41b4ce45f181d4088f12815325cd9b2dc00b74
cedrospl <58654169+cedrospl@users.noreply.github.com>
2021-01-19 19:41:47 +0100

    Delete PycharmProjects directory

--- PycharmProjects/Python_na_start.py/4. Rozdzial 5.3.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-print("Liczba argumentow przekazanych do programu: ", len(sys.argv))
-print("Wszystkie argumenty to: ", str(sys.argv))
-print("Nazwa programu to: ", sys.argv[0])
-print("Wynik dodawania parametrow to: ", sys.argv[1] + sys.argv[2])
